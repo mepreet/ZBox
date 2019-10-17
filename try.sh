3edcee35ef646bf30084d499271ace5b10da2000
mepreet <me.preetpreet@gmail.com>
2019-10-17 14:17:46 -0400

    Thu 17 Oct 2019 02:17:46 PM EDT

--- try.sh
+++ try.sh
@@ -11,15 +11,24 @@
 
 #-----------------------------------------------------------------------------
 
+echo ':::::::::  :::::::::    ::::::::   :::    ::: '
+echo '     :+:   :+:    :+:  :+:    :+:  :+:    :+: '
+echo '    +:+    +:+    +:+  +:+    +:+   +:+  +:+  '
+echo '   +#+     +#++:++#+   +#+    +:+    +#++:+   '
+echo '  +#+      +#+    +#+  +#+    +#+   +#+  +#+  '
+echo ' #+#       #+#    #+#  #+#    #+#  #+#    #+# '
+echo '#########  #########    ########   ###    ### '
 
-: <<'END'
+
+
 declare -A tools=(
 
 	["VMWare"]="https://www.vmware.com/go/getplayer-linux"
-	["OpenVPN"]="https://vpn.chiseclab.com/?src=connect"
 	["GoLang"]="https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz"
+	["NoMachine"]="https://download.nomachine.com/download/6.8/Linux/nomachine_6.8.1_1_amd64.deb"	
 	["Kerbrute"]="github.com/ropnop/kerbrute"
 	["Gowitness"]="github.com/sensepost/gowitness"
+	
 	["Impacket"]="https://github.com/SecureAuthCorp/impacket"
 	["CrackMapExec"]="https://github.com/byt3bl33d3r/CrackMapExec"
 	["LDAPDomaindump"]="https://github.com/dirkjanm/ldapdomaindump"
@@ -27,9 +36,10 @@ declare -A tools=(
 	["KinitHorBrute"]="https://raw.githubusercontent.com/ropnop/kerberos_windows_scripts/master/kinit_horizontal_brute.sh"
 	["EyeWitness"]="https://github.com/FortyNorthSecurity/EyeWitness"
 	["Nessus"]="https://www.tenable.com/downloads/nessus"
-	["NoMachine"]="https://www.nomachine.com/download/download&id=3"
+	["OpenVPN"]="https://vpn.chiseclab.com/?src=connect"
+	
 )
-
+: <<'END'
 declare -A yesORno=(
 
 	["VMWare"]="Y"
@@ -47,49 +57,47 @@ declare -A yesORno=(
 	["NoMachine"]="Y"
 )
 
-#-------------------------------------------------------------------------------------------
-
-# Set the debugger mode ON
-set -x
 
 #-------------------------------------------------------------------------------------------
 
 # Enable the ssh and edit 'sshd_config' file
 
-file_path='/etc/ssh/sshd_config'
-update-rc.d ssh enable
-sed -i.bak s/"#PermitRootLogin prohibit-password"/"PermitRootLogin yes"/ $file_path
+
+	file_path='/etc/ssh/sshd_config'
+	update-rc.d ssh enable
+	sed -i.bak s/"#PermitRootLogin prohibit-password"/"PermitRootLogin yes"/ $file_path
 
 
 #-------------------------------------------------------------------------------------------
 
 # checkpoint whether going full install or manual 
 
-hardWay=""
-read -p "Do you want to go Manually [y] or Hail Mary [n]: (y/n) => " hardWay
-until [ $hardWay == "y" -o $hardWay == "Y" -o $hardWay == "n" -o $hardWay == "N" ]
-do
-	echo "Invalid input try again"
-  	read -p "Do you want to go Manually [y] or Hail Mary [n]: (y/n) => " hardWay
-done
 
-if [ "$hardWay" == "y" -o "$hardWay" == "Y" ] ; then
-	for i in "${!yesORno[@]}"
-	do	
-		yORn=""
-		read -p "${i} select it (y/n): " yORn
-		until [ $yORn == "y" -o $yORn == "Y" -o $yORn == "n" -o $yORn == "N" ]
-		do
-			echo "Invalid input try again"
-  			read -p "${i} select it (y/n): " yORn
-		done
+	read -p "Do you want to go Manually [y] or Hail Mary [n]: (y/n) => " hardWay
+	until [ $hardWay == "y" -o $hardWay == "Y" -o $hardWay == "n" -o $hardWay == "N" ]
+	do
+		echo "Invalid input try again"
+  		read -p "Do you want to go Manually [y] or Hail Mary [n]: (y/n) => " hardWay
+	done
 
- 		yesORno[$i]=$yORn
-fi
+	if [ "$hardWay" == "y" -o "$hardWay" == "Y" ] ; then
+		for i in "${!yesORno[@]}"
+		do	
+			yORn=""
+			read -p "${i} select it (y/n): " yORn
+			until [ $yORn == "y" -o $yORn == "Y" -o $yORn == "n" -o $yORn == "N" ]
+			do
+				echo "Invalid input try again"
+	  			read -p "${i} select it (y/n): " yORn
+			done
+
+	 		yesORno[$i]=$yORn
+	fi
 END
 #-------------------------------------------------------------------------------------------
-spinner=( "|" "/" "-" "\\" )
-spin(){
+
+buffer(){
+	spinner=( "|" "/" "-" "\\" )
 	while kill -0 $1 >/dev/null 2>&1
   	do
     		for i in ${spinner[@]};
@@ -106,16 +114,16 @@ spin(){
 # Any New tool needed to be written in a seperate function 
 #-------------------------------------------------------------------------------------------
 
-success_msg="Installation Completed"
+
 
 # function for VMWare
 VMWare() {
 	echo "******************* VMWare *******************"
 	VMWare_bundle="/root/Downloads/VMWareZbox.bundle"
-	curl -L -o $VMWare_bundle --create-dirs https://www.vmware.com/go/getplayer-linux -s &
-	save_pid=$!; spin $save_pid Downloading..... "Download Completed....."
+	curl -L -o $VMWare_bundle --create-dirs ${tools["VMWare"]} -s &
+	buffer $! Downloading..... "Download Completed....."
 	chmod +x $VMWare_bundle ; $VMWare_bundle > /dev/null 2>&1 &
-	save_pid=$!; spin $save_pid Installing..... "Installation Completed....."
+	buffer $s! Installing..... "Installation Completed....."
 	return 0
 }
 
@@ -124,14 +132,39 @@ GoLang() {
 
 	echo "******************* GoLang *******************"
 	apt install golang > /dev/null 2>&1 & 
-	save_pid=$!; spin $save_pid Installing..... $(success_msg)
-	 
+	buffer $! Installing..... "Installation Completed....."
+	echo 'export PATH=$PATH:/root/go' >> /etc/profile ; source /etc/profile
+	echo 'export PATH=$PATH:/root/go' >> /root/.profile; source /root/.profile
+	
+	
 }
 
+# function for NoMachine
+NoMachine() {
+	echo "******************* NoMachine *******************"
+	Nomachine_deb="/root/Downloads/NoMachine.deb"
+	curl -o $Nomachine_deb --create-dirs ${tools["NoMachine"]} -s &
+	buffer $! Downloading..... "Download Completed....."
+	dpkg -i /root/Downloads/NoMachine.deb > /dev/null 2>&1 &
+	buffer $! Installing..... "Installation Completed....."
+}
 
 # function for Kerbrute
+kerbrute(){
+	go get ${tools["Kerbrute"]} &
+	buffer $! Downloading..... "Download Completed....."
+}
+
 # function for Gowitness
+Gowitness(){
+	go get -u ${tools["Gowitness"]} &
+	buffer $! Downloading..... "Download Completed....."
+}
+
 # function for Impacket
+Impacket(){
+	git clone ${tools["Impacket"]} /opt/impacket &
+}
 # function for CrackMapExec
 # function for LDAPDomaindump
 # function for SLUserNames
@@ -141,7 +174,7 @@ GoLang() {
 # function for NoMachine
 # function for OpenVPN
 
-GoLang
+Impacket
 #-------------------------------------------------------------------------------------------
 
 : <<'END'
